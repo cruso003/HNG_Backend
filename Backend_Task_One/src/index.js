dff46a24dd36b84d321c56f3954bcdde31e75825
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2024-07-01 00:16:36 +0300

    Initialize Backend repo and completed task one

---
+++ Backend_Task_One/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/hello', async (req, res) => {
+    const visitorName = req.query.visitor_name || 'Visitor';
+    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress || '8.8.8.8';
+    console.log(`Client IP: ${clientIp}`);
+
+    try {
+        const geoRes = await axios.get(`https://ipapi.co/${clientIp}/json/`);
+        console.log(geoRes.data);
+        const location = geoRes.data.city || 'Kampala';
+        const latitude = geoRes.data.latitude || '40.7128';
+        const longitude = geoRes.data.longitude || '-74.0060';
+        const tempRes = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+        console.log(tempRes.data);
+        const temperature = tempRes.data.current_weather.temperature;
+
+        res.json({
+            client_ip: clientIp,
+            location: location,
+            greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving location or weather data');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# HNG_Backend
+
+This repo contains my backend tasks at HNG Internship
