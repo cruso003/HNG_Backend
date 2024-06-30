c35bc305505e23bae2b86c1845b0c5d493b55e44
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2024-07-01 01:40:39 +0300

    Added default route

--- Backend_Task_One/src/index.js
+++ Backend_Task_One/src/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <title>Task 1 Response</title>
+            </head>
+            <body>
+                <p>Kindly click this link:</p>
+                <p><a href="https://hng-backend-awmi-dvxxabzyz-cruso003s-projects.vercel.app/api/hello">https://hng-backend-awmi-dvxxabzyz-cruso003s-projects.vercel.app/api/hello</a></p>
+                <p> to see task 1 response.</p>
+            </body>
+        </html>
+    `);
+});
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Mark';
@@ -11,9 +26,9 @@ app.get('/api/hello', async (req, res) => {
     try {
         const geoRes = await axios.get(`https://ipapi.co/${clientIp}/json/`);
         console.log(geoRes.data);
-        const location = geoRes.data.city || 'Kampalay';
-        const latitude = geoRes.data.latitude || '40.7128';
-        const longitude = geoRes.data.longitude || '-74.0060';
+        const location = geoRes.data.city || 'Kampala';
+        const latitude = geoRes.data.latitude || '0.3195080739328954';
+        const longitude = geoRes.data.longitude || '32.59220969763057';
         const tempRes = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
         console.log(tempRes.data);
         const temperature = tempRes.data.current_weather.temperature;
